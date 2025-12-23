645cfb3b471addcd267c40550a80198ab3791b19
Rafaellutaro <rafael.alves0_1@hotmail.com>
2025-12-23 15:46:47 -0300

    fixing some css and structure issues

--- src/components/Pets.tsx
+++ src/components/Pets.tsx
@@ -1,8 +1,10 @@
 import PetContainer from "./functions/petFunctions"
 import "../assets/css/Pets.css"
+import { TiThListOutline } from "react-icons/ti"
+import { TiThLargeOutline } from "react-icons/ti"
 
-function Pets () {
-    return(
+function Pets() {
+    return (
         <div className="box-container">
             <div className="left-container">
                 <h1>Categorias</h1>
@@ -22,28 +24,39 @@ function Pets () {
 
             <div className="right-container">
                 <div className="right-container-header">
-                    <h2>Ordernar</h2>
-                    <select name="order" id="order">
-                        <option value="">mais recentes etc...</option>
-                    </select>
 
-                    <h2>exibir</h2>
-                    <select name="show" id="show">
-                        <option value="">20 por pagina</option>
-                        <option value="">40 por pagina etc....</option>
-                    </select>
+                    <div className="order">
+                        <h2>Ordernar</h2>
+                        <select name="order" id="order">
+                            <option value="">mais recentes etc...</option>
+                        </select>
+                    </div>
 
-                    <h2>valor total de produtos achados/ usar uma variavel</h2>
+                    <div className="show">
+                        <h2>exibir</h2>
+                        <select name="show" id="show">
+                            <option value="">20 por pagina</option>
+                            <option value="">40 por pagina etc....</option>
+                        </select>
+                    </div>
+
+                    <div className="total-amount">
+                        <h2>valor total de produtos achados/ usar uma variavel</h2>
+                    </div>
 
                     {/* icons */}
+                    <div className="icons">
+                        <TiThListOutline />
+                        <TiThLargeOutline />
+                    </div>
                 </div>
 
                 <div className="pets-container-allpets">
                     {/* fetch get api to all pets based on params */}
 
-                    
-                        <PetContainer /> {/* using Pet container as a example, i need to create the correct api */}
-                    
+
+                    <PetContainer /> {/* using Pet container as a example, i need to create the correct api */}
+
                 </div>
             </div>
         </div>
